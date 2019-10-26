0d8e13fdb54c053abd84c970ee6f92baa5aa3f53
kostyniuk <kostyniuk7@gmail.com>
2019-10-26 09:08:55 +0300

    setState used

--- todolist/src/App.js
+++ todolist/src/App.js
@@ -1,25 +1,46 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import info from './info.json';
 
 function Greeting(props) {
   return (
-    <h1>Hi, {props.name}</h1>
+    <div>
+      <hr/>
+      <h1>Hi, {props.name}</h1>
+    </div>
   );
 }
 
-function App() {
-  const object = {name: 'Alex'};
-  console.log()
-  const names = info.data.map(el => <Greeting name = {el.name}/>);
-  console.log(names)
-  return (
-    <div className="App">
-      <h1>HI</h1>
-      <Greeting name = {names}/>
-    </div>
-  );
+
+class App extends React.Component {
+  
+  constructor() {
+    super();
+    this.state = {count: 2};
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick() {
+    this.setState(prev => {
+      return {
+        count: prev.count + 1
+      }
+    })
+  }
+
+  render() {
+    const namesComponents = info.data.map(el => <Greeting name = {el.name}/>);
+    console.log(namesComponents)
+    return (
+      <div className="App">
+        <h1>HI</h1>
+        {namesComponents}
+        <button onClick={this.handleClick}>Click me</button>
+        <h1>{this.state.count}</h1>
+      </div>
+    );
+  }
+  
 }
 
 export default App;
