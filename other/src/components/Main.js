162bf0042886d166abc88c6e65eb4d992be6dca7
kostyniuk <kostyniuk7@gmail.com>
2020-06-16 20:28:12 +0300

    playing around with the code

--- other/src/components/Main.js
+++ other/src/components/Main.js
@@ -2,14 +2,17 @@ import React, { useState, useEffect, useRef } from 'react';
 
 const Main = () => {
   const [count, setCount] = useState(0);
+  const [inp, setInp] = useState('');
+  const [text, setText] = useState('');
 
   let rerender = useRef(0);
-  console.log({rerender})
 
   useEffect(() => {
-    console.log('rerenders');
     rerender.current = rerender.current + 1;
-  }, []);
+    setTimeout(() => {
+      console.log(`got some data from an API: ${text}`)
+    }, 2000)
+  }, [text]);
 
   const increment = () => {
     setCount((prev) => prev + 1);
@@ -19,13 +22,21 @@ const Main = () => {
     setCount((prev) => prev - 1);
   };
 
+  const parseText = () => {
+    setText(inp)
+  }
+
   return (
     <div>
       <h2>Main Page</h2>
       <h3>Rerender: {JSON.stringify(rerender.current, null, 2)}</h3>
       <h3>Count: {count}</h3>
+      <h3>Input: {inp}</h3>
+      <h3>Text: {text}</h3>
       <button onClick={() => increment()}>+</button>
       <button onClick={() => decrement()}>-</button>
+      <input type='text' value={inp} onChange={e => setInp(e.target.value)} />
+      <button onClick={() => parseText()}>Submit</button>
     </div>
   );
 };
